f9f0817745f084ef1a84db654024558560b5da2d
Steven <stevenalfred93@yahoo.com>
2018-04-19 23:04:32 -0400

    Basic Layout

---
+++ Swoosh/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  Swoosh
+//
+//  Created by Steven on 4/19/18.
+//  Copyright © 2018 Steven Santiago. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
